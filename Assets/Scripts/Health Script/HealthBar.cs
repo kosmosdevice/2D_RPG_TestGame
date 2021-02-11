cdde0d1efa414e59976ed0942e1457bd5e24ec4d
kosmosdevice <44175862+kosmosdevice@users.noreply.github.com>
2021-02-11 10:58:13 +0100

    Health bar and merge
    Added Oskars' heath bar script and merged versions.

---
+++ Assets/Scripts/Health Script/HealthBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
+
+
+/* För att sätta in i ett player script!
+ * 
+ * public int maxHealth = 100;
+ * public int currentHealth;
+ * 
+ * public HealthBar healthBar;
+ * 
+ * void Start()
+ * {
+ *      currentHealth = maxHealth;
+ *      healthBar.SetMaxHealth(maxHealth);
+ * }
+ * 
+ * void Update()
+ * {
+ *      if(Vilkoret hur man ska ta skada)
+ *      {
+ *          TakeDamage(10);
+ *      }
+ * }
+ * 
+ * void TakeDamage(int damage)
+ * {
+ *      currentHealth -= damage;
+ *      healthBar.SetHealth(currentHealth);
+ * }
+ */
