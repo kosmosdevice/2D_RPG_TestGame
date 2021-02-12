50d5992628bde41114c1121ff1f1b80bce219df0
kosmosdevice <44175862+kosmosdevice@users.noreply.github.com>
2021-02-12 17:34:40 +0100

    Added skeleton enemy with animations.

---
+++ Assets/Scripts/MiniCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    void FixedUpdate()
+    {
+
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+
+    }
+}
