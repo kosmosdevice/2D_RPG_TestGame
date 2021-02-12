1112641f6e0a021840cd0de2b9df00e35f469579
kosmosdevice <44175862+kosmosdevice@users.noreply.github.com>
2021-02-12 15:34:19 +0100

    Added area transitions

--- Assets/Scripts/AreaTransitions.cs
+++ Assets/Scripts/AreaTransitions.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class AreaTransitions : MonoBehaviour
 {
+    private CameraController cam;
+
+    public Vector2 newMinPos;
+    public Vector2 newMaxPos;
+    public Vector3 movePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main.GetComponent<CameraController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            cam.minPosition = newMinPos;
+            cam.maxPosition = newMaxPos;
+            other.transform.position += movePlayer;
+        }
+
     }
 }
