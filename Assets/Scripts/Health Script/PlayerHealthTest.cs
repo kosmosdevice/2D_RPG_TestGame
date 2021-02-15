c8942ef55c37883ebe8eb546ee0e8e482a6e2def
kosmosdevice <44175862+kosmosdevice@users.noreply.github.com>
2021-02-15 16:51:27 +0100

    Added player hurt flash.

--- Assets/Scripts/Health Script/PlayerHealthTest.cs
+++ Assets/Scripts/Health Script/PlayerHealthTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHelathTest : MonoBehaviour
+public class PlayerHealthTest : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -7,17 +7,53 @@ public class HealthManager : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    private bool flashActive;
+    [SerializeField]
+    private float flashLength = 0f;
+    private float flashCounter = 0f;
+    private SpriteRenderer playerSprite;
+
     public HealthBar healthBar;
 
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-       
+       if (flashActive)
+        {
+            if(flashCounter > flashLength * 0.99f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else if (flashCounter > flashLength * 0.82f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            } else if (flashCounter > flashLength * 0.66f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else if (flashCounter > flashLength * 0.49f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            } else if (flashCounter > flashLength * 0.33f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else if (flashCounter > flashLength * 0.16f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            } else if (flashCounter > 0)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            } else
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+                flashActive = false;
+            }
+            flashCounter -= Time.deltaTime;
+        }
     }
 
     public void TakeDamage(int damage)
@@ -25,6 +61,8 @@ public class HealthManager : MonoBehaviour
 
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+        flashActive = true;
+        flashCounter = flashLength;
 
         if(currentHealth <= 0)
         {
